5d0dacb4ece4351631308411b88766452716f10e
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-10-04 13:50:03 -0700

    sticky navbar added

--- client/src/components/Navbar/Navbar.js
+++ client/src/components/Navbar/Navbar.js
@@ -4,9 +4,9 @@ import "./Navbar.css";
 
 // Depending on the current path, this component sets the "active" class on the appropriate navigation link item
 const Navbar = props => (
-  <nav className="navbar navbar-expand-lg navbar-light bg-light">
+  <nav className="navbar navbar-default navbar-fixed-top">
     <Link className="navbar-brand" to="/">
-      Pupster
+  ENG Share
     </Link>
     <div>
       <ul className="navbar-nav">
@@ -19,7 +19,7 @@ const Navbar = props => (
                 : "nav-link"
             }
           >
-            About
+            Home
           </Link>
         </li>
         <li className="nav-item">
