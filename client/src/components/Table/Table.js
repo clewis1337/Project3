193db4078517b4a0a1d7c5759a345dec8f68f5ca
Craig Lewis <clewis1337@gmail.com>
2018-10-05 14:50:58 -0700

    Sorry kayla, temporarily removed bootstrap table.  Using DirectoryTable instead

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -1,17 +1,18 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import "./Table.css";
-import {TableHeaderColumn, BootstrapTable} from 'react-bootstrap';
+// import {TableHeaderColumn, BootstrapTable} from 'react-bootstrap';
 
 
 class Table extends React.Component {
     render() {
       return (
-        <BootstrapTable data={ products }>
-          <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
-          <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
-          <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
-        </BootstrapTable>
+        // <BootstrapTable data={ products }>
+        //   <TableHeaderColumn dataField='id' isKey>Product ID</TableHeaderColumn>
+        //   <TableHeaderColumn dataField='name'>Product Name</TableHeaderColumn>
+        //   <TableHeaderColumn dataField='price'>Product Price</TableHeaderColumn>
+        // </BootstrapTable>
+        <div></div>
       );
     }
   }
