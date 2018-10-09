87a90aad3042c4a74437f84dfe732a12cfaac0b8
KBPF <anfootball@live.com>
2018-10-08 21:48:49 -0700

    Mongo Update

---
+++ models/User.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+// Define movie schema
+const userSchema = new Schema({
+    email: {
+    type: String,
+    default: ''
+    },
+    userName: {
+    type: String,
+    default: ''
+    },
+    password: {
+        type: String,
+        default: ''
+    },
+    isDeleted: {
+    type: Boolean,
+    default: false
+    },
+ 
+ 
+ });
+
+// Export Mongoose model
+const User = mongoose.model("User", userSchema);
+
+module.exports = User;
