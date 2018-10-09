8b6fe433b6afada5c6064f38fd27f75c94b2a896
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-10-09 12:00:33 -0700

    pop out

--- client/src/components/Forum/Forum3.js
+++ client/src/components/Forum/Forum3.js
@@ -1,12 +1,12 @@
 import React from "react";
-import "./Forum.css";
+import "./Forum3.css";
 import { render } from "react-dom";
 import { Card, CardHeader, CardBody, CardFooter, ImageHeader } from "react-simple-card";
  
 const Forum = () => (
   <Card>
-    <ImageHeader imageSrc="http://via.placeholder.com/600x250" />
-    <CardBody>Body</CardBody>
+     <img id="cover" src="/images/books/3rdGrade.jpg" />  
+    <CardBody></CardBody>
     <CardFooter>Footer</CardFooter>
   </Card>
 );
