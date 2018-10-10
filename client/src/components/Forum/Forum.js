efe906e4c4d22265238aef08b5dd4a6f0ace32fc
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-10-10 15:03:31 -0700

    forum css

--- client/src/components/Forum/Forum.js
+++ client/src/components/Forum/Forum.js
@@ -7,17 +7,28 @@ const Forum = (props) => (
     
     {props.PostData.map(eachPost =>(
     <div className="postWrapper">
+
+    <div class="container">
+    <div class="row">
+    <div class="col-lg-6">
         <div className="AuthorSection">
-            {eachPost.author}
-            <img src={eachPost.authorAvatar}/>
+          <div className="author">  {eachPost.author}</div>
+         <div className="img1">   <img src={eachPost.authorAvatar}/> </div>
         </div>
+        </div>
+    
+        <div class="col-lg-6">
         <div className="ContentSection">
             {eachPost.date}
             {eachPost.content}
             <br/>
             {eachPost.link}
         </div>
+        </div>
     </div>
+    </div>
+    </div>
+
     ))}
     </div>
 );
