a496ebf84114b857e69bbb4ac83eaa65bb95475b
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-10-11 14:24:00 -0700

    added rating to forum

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -11,42 +11,42 @@ const Table = () => (
     <div class="cards">
     <div class="grade3">
     <img id="cover" src="/images/books/3rdGrade.jpg" />
-    <div class="link"><Link to="/grade3">Grade 3 </Link>
+    <div class="linklink"><Link to="/grade3">Grade 3 </Link>
     <CardBody></CardBody>
     </div>
     </div>
 
     <div class="grade4">
     <img id="cover" src="/images/books/4thGrade.jpg" />
-    <div class="link"><Link to="/grade4">Grade 4 </Link>
+    <div class="linklink"><Link to="/grade4">Grade 4 </Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
     </div>
 
 <div class="grade5">
 <img id="cover" src="/images/books/5thGrade.jpg" />
-    <div class="link"><Link to="/grade5">Grade 5</Link>
+    <div class="linklink"><Link to="/grade5">Grade 5</Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
 
 <div class="grade6">
 <img id="cover" src="/images/books/6thGrade.jpg" />
-    <div class="link"><Link to="/grade6">Grade 6</Link>
+    <div class="linklink"><Link to="/grade6">Grade 6</Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
 
 <div class="grade7">
 <img id="cover" src="/images/books/7thGrade.jpg" />
-    <div class="link"><Link to="/grade7">Grade 7</Link>
+    <div class="linklink"><Link to="/grade7">Grade 7</Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
 
 <div class="grade8">
 <img id="cover" src="/images/books/8thGrade.jpg" />
-    <div class="link"><Link to="/grade8">Grade 8</Link>
+    <div class="linklink"><Link to="/grade8">Grade 8</Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
