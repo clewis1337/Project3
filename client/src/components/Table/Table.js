147582541d656517dc447b323ff724df6f97e46f
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-10-11 15:24:46 -0700

    links

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -10,43 +10,48 @@ const Table = () => (
 <div container>
     <div class="cards">
     <div class="grade3">
-    <img id="cover" src="/images/books/3rdGrade.jpg" />
+    <a href="/grade3">
+    <img id="cover" src="/images/books/3rdGrade.jpg" /></a>
     <div class="linklink"><Link to="/grade3">Grade 3 </Link>
-    <CardBody></CardBody>
     </div>
     </div>
 
     <div class="grade4">
-    <img id="cover" src="/images/books/4thGrade.jpg" />
+    <a href="https://www.w3schools.com">
+    <img id="cover" src="/images/books/4thGrade.jpg" /></a>
     <div class="linklink"><Link to="/grade4">Grade 4 </Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
     </div>
 
 <div class="grade5">
-<img id="cover" src="/images/books/5thGrade.jpg" />
+<a href="https://www.w3schools.com">
+<img id="cover" src="/images/books/5thGrade.jpg" /></a>
     <div class="linklink"><Link to="/grade5">Grade 5</Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
 
 <div class="grade6">
-<img id="cover" src="/images/books/6thGrade.jpg" />
+<a href="https://www.w3schools.com">
+<img id="cover" src="/images/books/6thGrade.jpg" /></a>
     <div class="linklink"><Link to="/grade6">Grade 6</Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
 
 <div class="grade7">
-<img id="cover" src="/images/books/7thGrade.jpg" />
+<a href="https://www.w3schools.com">
+<img id="cover" src="/images/books/7thGrade.jpg" /></a>
     <div class="linklink"><Link to="/grade7">Grade 7</Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
 
 <div class="grade8">
-<img id="cover" src="/images/books/8thGrade.jpg" />
-    <div class="linklink"><Link to="/grade8">Grade 8</Link>
+<a href="https://www.w3schools.com">
+<img id="cover" src="/images/books/8thGrade.jpg" /> </a>
+    <div class="linklink"><Link to="/grade8">Grade 8</Link> 
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
     </div>
 </div>
