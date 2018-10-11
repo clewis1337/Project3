9bd15cdae403c5c90358cf8bd4f246d246b922ff
Kayla Goldner <Kayla.Goldner@precisiondxlab.com>
2018-10-11 15:35:06 -0700

    added link for grade levels

--- client/src/components/Table/Table.js
+++ client/src/components/Table/Table.js
@@ -10,50 +10,34 @@ const Table = () => (
 <div container>
     <div class="cards">
     <div class="grade3">
-    <a href="/grade3">
+    <a href="/topic/id/:id">
     <img id="cover" src="/images/books/3rdGrade.jpg" /></a>
-    <div class="linklink"><Link to="/grade3">Grade 3 </Link>
-    </div>
     </div>
 
     <div class="grade4">
-    <a href="https://www.w3schools.com">
+    <a href="/topic/id/:id">
     <img id="cover" src="/images/books/4thGrade.jpg" /></a>
-    <div class="linklink"><Link to="/grade4">Grade 4 </Link>
     {/* <CardBody>Click for Lesson Plans</CardBody> */}
-    </div>
     </div>
 
 <div class="grade5">
-<a href="https://www.w3schools.com">
+<a href="/topic/id/:id">
 <img id="cover" src="/images/books/5thGrade.jpg" /></a>
-    <div class="linklink"><Link to="/grade5">Grade 5</Link>
-    {/* <CardBody>Click for Lesson Plans</CardBody> */}
-    </div>
 </div>
 
 <div class="grade6">
-<a href="https://www.w3schools.com">
+<a href="/topic/id/:id">
 <img id="cover" src="/images/books/6thGrade.jpg" /></a>
-    <div class="linklink"><Link to="/grade6">Grade 6</Link>
-    {/* <CardBody>Click for Lesson Plans</CardBody> */}
-    </div>
 </div>
 
 <div class="grade7">
-<a href="https://www.w3schools.com">
+<a href="/grade7">
 <img id="cover" src="/images/books/7thGrade.jpg" /></a>
-    <div class="linklink"><Link to="/grade7">Grade 7</Link>
-    {/* <CardBody>Click for Lesson Plans</CardBody> */}
-    </div>
 </div>
 
 <div class="grade8">
-<a href="https://www.w3schools.com">
+<a href="/topic/id/:id">
 <img id="cover" src="/images/books/8thGrade.jpg" /> </a>
-    <div class="linklink"><Link to="/grade8">Grade 8</Link> 
-    {/* <CardBody>Click for Lesson Plans</CardBody> */}
-    </div>
 </div>
 </div>
 </div>
