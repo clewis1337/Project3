b522b93e86c74fb922ed9e83a6f8c59f74591314
KBPF <anfootball@live.com>
2018-10-11 18:29:34 -0700

    API

--- client/src/pages/LessonPlans.js
+++ client/src/pages/LessonPlans.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import Forum from "../components/Forum";
 import Jumbotron from "../components/Jumbotron";
 import Container from "../components/Container";
+import API from "../utils/API";
 
 const LessonPlans = props => {
     return (
--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,15 +1,20 @@
-// import axios from "axios";
+import axios from "axios";
 
-// // Export an object containing methods we'll use for accessing the Dog.Ceo API
-
-// export default {
-//   getRandomDog: function() {
-//     return axios.get("https://dog.ceo/api/breeds/image/random");
-//   },
-//   getDogsOfBreed: function(breed) {
-//     return axios.get("https://dog.ceo/api/breed/" + breed + "/images");
-//   },
-//   getBaseBreedsList: function() {
-//     return axios.get("https://dog.ceo/api/breeds/list");
-//   }
-// };
+export default {
+    // Gets all books
+    getPosts: function () {
+        return axios.get("/api/posts");
+    },
+    // Gets the book with the given id
+    getPost: function (id) {
+        return axios.get("/api/posts/" + id);
+    },
+    // Deletes the book with the given id
+    deletePost: function (id) {
+        return axios.delete("/api/posts/" + id);
+    },
+    // Saves a book to the database
+    savePost: function (postData) {
+        return axios.post("/api/posts", postData);
+    }
+};
