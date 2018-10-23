c831589d23bc72a8feaa91bd880b02f86ac13e8c
Craig Lewis <clewis1337@gmail.com>
2018-10-23 15:57:26 -0700

    herokupush1

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var mongojs = require("mongojs");
 const routes = require("./routes");
 const path = require("path");
 const app = express();
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT + 1 || 3001;
 
 // Define middleware here
 app.use(bodyParser.urlencoded({ extended: true }));
